197abb9d106f6dd2bca05ffb6e4b4e50f374392d
martjn <mingdev77@gmail.com>
2023-09-07 13:37:47 +0300

    added author name into single article

--- index.js
+++ index.js
@@ -47,6 +47,19 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get('/article/:slug', (req, res) => {
+  let query = `SELECT * from article WHERE slug="${req.params.slug}"`
+  let article
+  con.query(query, (err, result) => {
+    if(err) throw err;
+    article = result
+    console.log(article)
+    res.render('article', {
+      article: article
+    })
+  });
+});
+
 // app start
 app.listen(3000, () => {
   console.log("App is started at http://localhost:3000");
