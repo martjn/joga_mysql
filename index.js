6a073eeb5fa2cd850e375adaf853e0026080c1bd
martjn <mingdev77@gmail.com>
2023-09-07 12:28:21 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const path = require('path')
+const path = require("path");
+const hbs = require("express-handlebars");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
 
-// add template engine
-const hbs = require('express-handlebars');
-// setup template engine directory and files extensions
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
-app.engine('hbs', hbs.engine({
-  extname: 'hbs',
-  defaultLayout: 'main',
-  layoutsDir: __dirname + 'views/layouts/',
-}))
+app.use(express.static("public"));
 
-// setup static public directory
-app.use(express.static('public'))
+const mysql = require("mysql2");
 
-const mysql = require('mysql2')
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Ij6lS*H?ScwD',
-  database: 'joga_mysql',
-
+  host: "localhost",
+  user: "root",
+  password: "Ij6lS*H?ScwD",
+  database: "joga_mysql",
 });
 
-con.connect(function(err) {
-  if(err) throw err;
-  console.log('Connected to joga_mysql db')
-})
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to joga_mysql db");
+});
+
+app.get("/", (req, res) => {
+  let query = "SELECT * FROM article";
+  let articles = [];
+  con.query(query, (err, result) => {
+    if (err) throw err;
+    articles = result;
+    res.render("index", {
+      articles: articles,
+    });
+    console.log(articles);
+  });
+  res.render("index");
+});
 
 // app start
 app.listen(3000, () => {
-  console.log('App is started at http://localhost:3000')
-})
+  console.log("App is started at http://localhost:3000");
+});
